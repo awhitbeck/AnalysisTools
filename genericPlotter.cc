76a34900dbb40b9688942fbab56cc1ea1a294c3a
awhitbeck <whitbeck.andrew@gmail.com>
2016-01-29 15:55:46 -0600

    adding more variables to the histogram list

--- genericPlotter.cc
+++ genericPlotter.cc
@@ -23,21 +23,35 @@ int main(int argc, char** argv){
   TChain* t = buildChain("slimFiles.txt",sample,"analysisTree");
   
   dissectingJetsMET *ntuple = new dissectingJetsMET(t);
-
   weightProducer *weightProd = new weightProducer(ntuple,sample);
-  fillHisto *fill = new fillHisto(ntuple,200,400,10000,sample,"HT",weightProd);
   selectBaseline *select = new selectBaseline(ntuple);
 
   analyzer a(ntuple);
   a.addProcessor( select );
-  a.addProcessor( fill );
+
+  fillHisto *fillHT = new fillHisto(ntuple,200,500,2500,sample,"HT",weightProd);   a.addProcessor( fillHT );
+  fillHisto *fillMHT = new fillHisto(ntuple,200,200,1000,sample,"MHT",weightProd); a.addProcessor( fillMHT );
+  fillHisto *fillMET = new fillHisto(ntuple,200,200,1000,sample,"MET",weightProd); a.addProcessor( fillMET );
+  fillHisto *fillmT2 = new fillHisto(ntuple,200,200,2500,sample,"mT2",weightProd); a.addProcessor( fillmT2 );
+  fillHisto *fillalphaT = new fillHisto(ntuple,200,0,2,sample,"alphaT",weightProd); a.addProcessor( fillalphaT );
+  fillHisto *fillmRazor = new fillHisto(ntuple,200,200,4000,sample,"mRazor",weightProd); a.addProcessor( fillmRazor );
+  fillHisto *filldRazor = new fillHisto(ntuple,200,0,1,sample,"dRazor",weightProd); a.addProcessor( filldRazor );
+  fillHisto *fillmEff = new fillHisto(ntuple,200,400,4000,sample,"mEff",weightProd); a.addProcessor( fillmEff );
+  fillHisto *fillNJets = new fillHisto(ntuple,11,-0.5,10.5,sample,"NJets",weightProd); a.addProcessor( fillNJets );
+  fillHisto *fillNLeptons = new fillHisto(ntuple,4,-0.5,3.5,sample,"NLeptons",weightProd); a.addProcessor( fillNLeptons );
+  fillHisto *filldPhi = new fillHisto(ntuple,20,0.,3.1415,sample,"dPhi",weightProd); a.addProcessor( filldPhi );
+  fillHisto *filldEta = new fillHisto(ntuple,20,0.,5.,sample,"dEta",weightProd); a.addProcessor( filldEta );
+  fillHisto *fillsumJetMass = new fillHisto(ntuple,200,100.,1000.,sample,"sumJetMass",weightProd); a.addProcessor( fillsumJetMass );
+  fillHisto *fillleadJetPt = new fillHisto(ntuple,200,30,500,sample,"leadJetPt",weightProd); a.addProcessor( fillleadJetPt );
 
   a.looper();
 
-  fill->histo->Draw();
-
   TFile* outFile = new TFile("genericPlotter_"+sample+".root","UPDATE");
-  fill->histo->Write();
+  for( unsigned int iProc = 0 ; iProc < a.processorList.size() ; iProc++ ){
+    if( filler* myFiller = dynamic_cast<filler*>( a.processorList[iProc] ) ){
+      myFiller->histo->Write();
+    }
+  }
   outFile->Close();
 
 }  
--- mergePlot.sh
+++ mergePlot.sh
@@ -2,10 +2,10 @@
 
 source /cvmfs/cms.cern.ch/cmsset_default.sh
 
-hadd -f HTvalidation.root `xrdfsls -u /store/user/awhitbe1/DissectingJetsPlusMET/analysis/ | grep outFile | grep \.root`
-python stackPlots.py -s QCD -f HTvalidation.root
-python stackPlots.py -s Wjets -f HTvalidation.root
-python stackPlots.py -s znunu -f HTvalidation.root
-python stackPlots.py -s ttbar -f HTvalidation.root
+hadd -f genericPlotter.root `xrdfsls -u /store/user/awhitbe1/DissectingJetsPlusMET/analysis/ | grep genericPlotter | grep \.root`
+python stackPlots.py -s QCD -f genericPlotter.root
+python stackPlots.py -s Wjets -f genericPlotter.root
+python stackPlots.py -s znunu -f genericPlotter.root
+python stackPlots.py -s ttbar -f genericPlotter.root
 
 scp *Stack.png whitbeck@lxplus.cern.ch:www/.
