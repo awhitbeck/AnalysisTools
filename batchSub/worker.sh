612007bf681390ee174dcfc14a71196cc733cea3
awhitbeck <whitbeck.andrew@gmail.com>
2016-02-12 10:40:17 -0600

    fixing some bugs

--- batchSub/worker.sh
+++ batchSub/worker.sh
@@ -9,12 +9,14 @@ eval `scramv1 runtime -sh`
 pwd
 
 git clone https://github.com/awhitbeck/AnalysisTools.git
+outputDir=$(git rev-parse origin/master)
 
 cd AnalysisTools
 g++ `root-config --cflags --glibs` genericPlotter4Jets.cc -o genericPlotter4Jets.exe
 ls
 echo "RUNNING ANALYSIS"
-./genericPlotter.exe $1
+./genericPlotter4Jets.exe $1
 ls
 echo "COPYING OUTPUT"
-xrdcp genericPlotter_${1}.root root://cmseos.fnal.gov//store/user/awhitbe1/DissectingJetsPlusMET/analysis/genericPlotter4Jets_${1}.root
+#eosls /store/user/awhitbe1/GMSBstudies/skims/
+xrdcp genericPlotter4Jets_${1}.root root://cmseos.fnal.gov//store/user/awhitbe1/DissectingJetsPlusMET/analysis/genericPlotter4Jets_${1}.root
